851b22c4fd1b6605348f1ad1ae4133b28b693687
Olga Isaeva <helga.n.isaeva@gmail.com>
2019-02-26 22:32:12 +0300

    add game brain-even

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 [![Maintainability](https://api.codeclimate.com/v1/badges/07daf8905b8d5673d428/maintainability)](https://codeclimate.com/github/olgaisaeva/project-lvl1-s450/maintainability)
 [![Build Status](https://travis-ci.org/olgaisaeva/project-lvl1-s450.svg?branch=master)](https://travis-ci.org/olgaisaeva/project-lvl1-s450)
+
+## brain-even installation
+
+[![asciicast](https://asciinema.org/a/oHATQkLVYtYGHqt1mEGvACfJ1.svg)](https://asciinema.org/a/oHATQkLVYtYGHqt1mEGvACfJ1)
--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
 import readlineSync from 'readline-sync';
 
-const welcomeUser = () => {
-  console.log('Welcome to the Brain Games!\n');
+const isEven = num => num % 2 === 0;
+const getRandomIntBetweenMinAndMax = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 
+const makeGame = () => {
+  console.log('Welcome to the Brain Games!\nAnswer "yes" if number even otherwise answer "no".\n');
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
+  console.log(`Hello, ${userName}!\n`);
+
+  const iter = (counter) => {
+    if (counter === 0) {
+      console.log(`Congratulations, ${userName}!`);
+      return;
+    }
+
+    const randomNumber = getRandomIntBetweenMinAndMax(0, 1000);
+    console.log(`Question: ${randomNumber}`);
+    const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
+      iter(counter - 1);
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
+    }
+  };
+
+  const roundsCount = 3;
+  iter(roundsCount);
 };
 
-export default welcomeUser;
+export default makeGame;
